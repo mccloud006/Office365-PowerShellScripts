22469e7014c6cea20630d4a2dc14778e25dea0ec
DigitalRidder <richard.ridder@digital.homeoffice.gov.uk>
2016-03-17 10:59:13 +0000

    Initial upload

---
+++ Create_New_user.ps1
@@ -0,0 +1,36 @@
+﻿#set the location of the CSV file that has the user details
+$path = "C:\o365\office365users.csv" 
+
+#this sets the licence plan that we want to apply - use the command Get-MsolAccountSku to list the types of licences we have
+$server = "homeofficegovuk:ENTERPRISEWITHSCAL" 
+#Specify the plans that we want to disable on a user account. To see a list of planes a user has, paste this command into the console:
+#Get-MsolUser -UserPrincipalName richard.ridder@digital.homeoffice.gov.uk | Select-Object -ExpandProperty Licenses | Select-Object -ExpandProperty ServiceStatus
+$LO = New-MsolLicenseOptions -AccountSkuId "homeofficegovuk:ENTERPRISEWITHSCAL" -DisabledPlans "YAMMER_ENTERPRISE", "SWAY"
+
+import-csv $path | foreach { 
+Write-Host "Creating new account for:" $_.displayname
+New-Msoluser -userPrincipalName $_.UserPrincipalName -displayname $_.displayname -firstname $_.firstname -lastname $_.lastname -password $_.Password -LicenseAssignment $server -LicenseOptions $LO -usagelocation "GB"#| set-msoluserlicense -addlicenses "$server"
+Write-Host "Applying Mailbox features:"
+#Enable or Disable OWA,IMAP, MAPI and POP3
+Set-CASMailbox $_.UserPrincipalName -OWAEnabled $True -PopEnabled $False -MAPIEnabled $false -ImapEnabled $false -ActiveSyncEnabled $false
+#Enforce MFA on user account
+
+$mf= New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+$mf.RelyingParty = "*"
+$mfa = @($mf)
+
+Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $mfa
+ 
+
+#Add user to the Home Office Digital group
+
+$user = Get-Msoluser -UserPrincipalName $_.Userprincipalname | select Objectid 
+Add-MsolGroupMember -groupObjectid ‘6d743fec-af0e-4c94-be4d-abfc603e852b’ -GroupMemberObjectId $user.ObjectId -GroupMemberType User
+
+#Enable litigation holdget
+Set-Mailbox $_.UserPrincipalName -LitigationHoldEnabled $true
+
+#Send a welcome email to the user.
+
+}
+
---
+++ mailbox_report.ps1
@@ -0,0 +1,83 @@
+﻿
+# Enable to generate .csv
+Get-CASMailbox -ResultSize Unlimited |Select Name,ActiveSyncEnabled,OWAEnabled,PopEnabled,ImapEnabled,MapiEnabled |Export-CSV "C:\o365\Mail_Enabled.csv"
+
+# Enable to generate HTML report
+#Get-CASMailbox *@digital.homeoffice.gov.uk -ResultSize Unlimited |Select Name,ActiveSyncEnabled,OWAEnabled,PopEnabled,ImapEnabled,MapiEnabled |ConvertTo-Html -As Table | Out-File mail_enabled.html
+
+#Invoke-Item mail_enabled.html 
+
+#Import-Module MSOnline
+
+$head = @"
+<!DOCTYPE HTML PUBLIC  HTML 4.01 Frameset//EN” “http://www.w3.org/TR/html4/frameset.dtd” &gt;
+<html><head><title>Mailbox Enabled Features</title><meta http-equiv=”refresh” content=”120″ />
+<style type="text/css">
+<!–
+body {
+font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
+}
+ 
+#report { width: 835px; }
+ 
+table{
+border-collapse: collapse;
+border: none;
+font: 10pt Verdana, Geneva, Arial, Helvetica, sans-serif;
+color: black;
+margin-bottom: 10px;
+}
+ 
+table td{
+font-size: 12px;
+padding-left: 0px;
+padding-right: 20px;
+text-align: left;
+}
+ 
+table th {
+font-size: 12px;
+font-weight: bold;
+padding-left: 0px;
+padding-right: 20px;
+text-align: left;
+}
+ 
+h2{ clear: both; font-size: 130%;color:#354B5E; }
+ 
+h3{
+clear: both;
+font-size: 75%;
+margin-left: 20px;
+margin-top: 30px;
+color:#475F77;
+}
+ 
+p{ margin-left: 20px; font-size: 12px; }
+ 
+table.list{ float: left; }
+ 
+table.list td:nth-child(1){
+font-weight: bold;
+border-right: 1px grey solid;
+text-align: right;
+}
+ 
+table.list td:nth-child(2){ padding-left: 7px; }
+table tr:nth-child(even) td:nth-child(even){ background: #BBBBBB; }
+table tr:nth-child(odd) td:nth-child(odd){ background: #F2F2F2; }
+table tr:nth-child(even) td:nth-child(odd){ background: #DDDDDD; }
+table tr:nth-child(odd) td:nth-child(even){ background: #E5E5E5; }
+div.column { width: 320px; float: left; }
+div.first{ padding-right: 20px; border-right: 1px grey solid; }
+div.second{ margin-left: 30px; }
+table{ margin-left: 20px; }
+</style>
+</head> 
+"@
+$date = Get-Date
+   
+#Get-CASMailbox *@digital.homeoffice.gov.uk -ResultSize Unlimited |Select Name,ActiveSyncEnabled,OWAEnabled,PopEnabled,ImapEnabled,MapiEnabled |ConvertTo-Html -as table -Head $head -PreContent “<P>The current list of users and which mailbox features are enabled.</P>” -PostContent “<h3><br>Created on $date</h3>” |Out-File c:\o365\mail_enabled.html
+#Invoke-Item C:\O365\mail_enabled.html
+
+#Save to our reports section
